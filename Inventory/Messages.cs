b3bd1e12796b0567b4cb0c3644ddc14a4ab225a7
Patrick Fifield <plfifield9@yahoo.com>
2023-08-08 20:36:30 -0400

    Credits for 2.1.2

--- Inventory/Messages.cs
+++ Inventory/Messages.cs
@@ -82,7 +82,7 @@ namespace FF4FalconDive.Inventory
 					msgStrings.Where(c => c.id == "E0030_00_353_a_04").Single().msgString = @"Beta Helpers\n";
 					msgStrings.Where(c => c.id == "E0030_00_353_a_05").Single().msgString = @"Antidale\nEngidave\nFleury14\nInfinious\nInven\nTybalt\nSyconawt\nwarlink05\n\n";
 					msgStrings.Where(c => c.id == "E0030_00_353_a_06").Single().msgString = @"Other contributors\n";
-					msgStrings.Where(c => c.id == "E0030_00_353_a_07").Single().msgString = @"CasualGeekDave\nBGG02\nRegasin";
+					msgStrings.Where(c => c.id == "E0030_00_353_a_07").Single().msgString = @"CasualGeekDave\nBGG02\nRegasin\nHolySmith";
 					msgStrings.Where(c => c.id == "E0074_06_354_a_01").Single().msgString = @"Special Thanks\n";
 					msgStrings.Where(c => c.id == "E0074_06_354_a_02").Single().msgString = @"mcgrew\nAlbeoris\nSchalaKitty\nSilvris\n\n";
 					msgStrings.Where(c => c.id == "E0074_06_354_a_03").Single().msgString = @"Based off of and inspired by\nFinal Fantasy IV:  Free Enterprise\nwww.ff4fe.com\n";
